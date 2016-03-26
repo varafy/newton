3478b1407d5db284fa602378811a133c4e2d0fb0
Justin Sharp <justin.d.sharp@gmail.com>
2016-03-25 18:40:50 -0600

    react is now loading, the tutorial had wrong versions

---
+++ app/components/hello.jsx
@@ -0,0 +1,17 @@
+/**
+ * Created by jsharp on 2016-03-25.
+ *
+ *      Varafy Inc.
+ *
+ */
+
+'use strict';
+
+const React = require('react');
+
+module.exports = React.createClass({
+  displayName: 'HelloReact',
+  render: () => {
+    return <div>Hello React</div>;
+  },
+});
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * Created by jsharp on 2016-03-25.
+ *
+ *      Varafy Inc.
+ *
+ */
+
+'use strict';
+var ReactDOM = require('react-dom');
+var Hello = require('./components/Hello');
+
+ReactDOM.render(<Hello />, document.getElementById('content'));
